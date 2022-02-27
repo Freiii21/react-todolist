33fadf299601d8b292e5a259970704823f0094d9
Freiii21 <mrfreiii@gmail.com>
2022-02-27 22:05:16 +0300

    #42 lesson_16_4

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -9,12 +9,33 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {useFormik} from 'formik';
 
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
+
+
 export const Login = () => {
     const formik = useFormik({
         initialValues: {
             email: '',
             password: '',
             rememberMe: false
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (!values.password) {
+                errors.password = 'Required';
+            } else if (values.password.length<3) {
+                errors.password = 'Password length must be 3 or longer';
+            }
+            return errors;
         },
         onSubmit: values => {
             alert(JSON.stringify(values));
@@ -44,6 +65,7 @@ export const Login = () => {
                             onChange={formik.handleChange}
                             value={formik.values.email}
                         />
+                        {formik.errors.email && <div style={{color: "red"}}>{formik.errors.email}</div>}
                         <TextField type="password"
                                    label="Password"
                                    margin="normal"
@@ -51,6 +73,7 @@ export const Login = () => {
                                    onChange={formik.handleChange}
                                    value={formik.values.password}
                         />
+                        {formik.errors.password && <div style={{color: "red"}}>{formik.errors.password}</div>}
                         <FormControlLabel label={'Remember me'}
                                           control={<Checkbox name="rememberMe"
                                                              onChange={formik.handleChange}
